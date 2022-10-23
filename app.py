7e54d78a713fb28c887ad974f75bdce6e3b50cf2
DanielAntonioMatuz <183441@ids.upchiapas.edu.mx>
2022-10-23 13:03:38 -0500

    add ssl

--- app.py
+++ app.py
@@ -119,4 +119,4 @@ jwt = JWTManager(app)
 CORS(app)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", ssl_context=('cert.pem', 'key.pem'))
